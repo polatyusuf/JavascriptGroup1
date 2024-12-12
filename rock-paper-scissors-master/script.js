4c7169163123f439cd9a9d8939760c2497ece97e
Yusuf Polat <60508448+polatyusuf@users.noreply.github.com>
2024-12-12 19:47:30 +0100

    Update script.js
    the score will be continued after a refresh or a reset

--- rock-paper-scissors-master/script.js
+++ rock-paper-scissors-master/script.js
@@ -1,52 +1,78 @@
-let playerScore = 0;
-let computerScore = 0;
-
 function playGame(humanChoice) {
-    // Array
-    const choices = ["rock", "paper", "scissors"];
-    const computerChoice = choices[Math.floor(Math.random() * choices.length)];
-    const imgPath = "images/";
+  let gameScore = {
+    playerScore: 0,
+    computerScore: 0,
+  };
 
-    document.getElementById("compChoice").innerText = `Computer choose: ${computerChoice}`; //Anzeige der Computerwahl auf der Webseite
-    console.log(`Computer choose: ${computerChoice}`); 
-    //document.getElementById("playerChoice").innerText=`You choose: ${humanChoice}`;
-    // Vergleich zwischen der wahl des Menschen un der Wahl de Computers
-   
-    let resultMessage;
-    if ((humanChoice === "scissors" && computerChoice === "paper") || 
-        (humanChoice === "paper" && computerChoice === "rock") || 
-        (humanChoice === "rock" && computerChoice === "scissors")) {
-        resultMessage = `${humanChoice} beats ${computerChoice}, You win!`;
-        playerScore++;
-    } 
-    else if (humanChoice === computerChoice) {
-        resultMessage = "No winner, it's a tie!";
-    } 
-    else {
-        resultMessage = `${computerChoice} beats ${humanChoice}, Computer wins!`;
-        computerScore++;
-    }
-    
+  let scoreItem = localStorage.getItem("score");
 
-    // // Anzeige des results auf der Webseite
-    document.getElementById("result").innerText = resultMessage;
+  if (!scoreItem) {
+    let gameScoreString = JSON.stringify(gameScore);
+    localStorage.setItem("score", gameScoreString);
+  } else {
+    let scoreItemObject = JSON.parse(scoreItem);
+    gameScore.computerScore = scoreItemObject.computerScore;
+    gameScore.playerScore = scoreItemObject.playerScore;
+  }
 
-    // Aktualisierung der Punktzahl aud der Webseite
-    document.getElementById("player").innerText = `Player: ${playerScore}`;
-    document.getElementById("computer").innerText = `Computer: ${computerScore}`;
+  // Array
+  const choices = ["rock", "paper", "scissors"];
+  const computerChoice = choices[Math.floor(Math.random() * choices.length)];
+  const imgPath = "images/";
 
-    // document.getElementById("playerImage").src = `${imgPath}${humanChoice}.svg`;
-    document.getElementById("playerImage").src = `${imgPath}${humanChoice}.svg`;
-    console.log(`${imgPath}${humanChoice}.svg`)
+  document.getElementById(
+    "compChoice"
+  ).innerText = `Computer choose: ${computerChoice}`; //Anzeige der Computerwahl auf der Webseite
+  console.log(`Computer choose: ${computerChoice}`);
+  //document.getElementById("playerChoice").innerText=`You choose: ${humanChoice}`;
+  // Vergleich zwischen der wahl des Menschen un der Wahl de Computers
 
+  let resultMessage;
+  if (
+    (humanChoice === "scissors" && computerChoice === "paper") ||
+    (humanChoice === "paper" && computerChoice === "rock") ||
+    (humanChoice === "rock" && computerChoice === "scissors")
+  ) {
+    resultMessage = `${humanChoice} beats ${computerChoice}, You win!`;
+    gameScore.playerScore = gameScore.playerScore + 1;
+  } else if (humanChoice === computerChoice) {
+    resultMessage = "No winner, it's a tie!";
+  } else {
+    resultMessage = `${computerChoice} beats ${humanChoice}, Computer wins!`;
+    gameScore.computerScore = gameScore.computerScore + 1;
+  }
+
+  // Update the localeStorage
+  gameScoreString = JSON.stringify(gameScore);
+  localStorage.setItem("score", gameScoreString);
+
+  // // Anzeige des results auf der Webseite
+  document.getElementById("result").innerText = resultMessage;
+
+  // Aktualisierung der Punktzahl aud der Webseite
+  document.getElementById(
+    "player"
+  ).innerText = `Player: ${gameScore.playerScore}`;
+  document.getElementById(
+    "computer"
+  ).innerText = `Computer: ${gameScore.computerScore}`;
+
+  // document.getElementById("playerImage").src = `${imgPath}${humanChoice}.svg`;
+  document.getElementById("playerImage").src = `${imgPath}${humanChoice}.svg`;
+  console.log(`${imgPath}${humanChoice}.svg`);
 }
 
 //Zurückstzen
 function resetGame() {
-    playerScore = 0;
-    computerScore = 0;
-    document.getElementById("player").innerText = "Player: 0";
-    document.getElementById("computer").innerText = "Computer: 0";
-    document.getElementById("result").innerText = "Choose an option to play!";
-    document.getElementById("compChoice").innerText = "";
+  let initScore = {
+    playerScore: 0,
+    computerScore: 0,
+  };
+
+  localStorage.setItem("score", JSON.stringify(initScore));
+
+  document.getElementById("player").innerText = "Player: 0";
+  document.getElementById("computer").innerText = "Computer: 0";
+  document.getElementById("result").innerText = "Choose an option to play!";
+  document.getElementById("compChoice").innerText = "";
 }
